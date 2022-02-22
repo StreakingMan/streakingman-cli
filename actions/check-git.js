795f67937d4b32599b191ad3e4906eba101cd90e
streakingman <z_max_y@163.com>
2022-02-22 16:42:44 +0800

    fix: 取消check-git入参

--- actions/check-git.js
+++ actions/check-git.js
@@ -1,11 +1,7 @@
 const {execSync} = require('child_process')
-const checkGit = (propName) => {
-    if (!propName || propName === 'username') {
-        console.log(`git config user.name ${execSync('git config user.name')}`)
-    }
-    if (!propName || propName === 'email') {
-        console.log(`git config user.email ${execSync('git config user.email')}`)
-    }
+const checkGit = () => {
+    console.log(`git config user.name ${execSync('git config user.name')}`)
+    console.log(`git config user.email ${execSync('git config user.email')}`)
 }
 
 module.exports = {
