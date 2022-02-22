3a00e9c2f6658219cbbe223d787424612efd6985
streakingman <z_max_y@163.com>
2022-02-22 17:12:39 +0800

    fix: set-git参数获取姿势

--- actions/set-git.js
+++ actions/set-git.js
@@ -1,25 +1,23 @@
 const {execSync} = require('child_process')
 const inquirer = require('inquirer')
 const setGit = async () => {
-    const name = await inquirer.prompt([
+    const {name, email} = await inquirer.prompt([
         {
             type: 'input',
             name: 'name',
             message: 'user.name',
             default: 'streakingman',
         },
-    ])
-    const email = await inquirer.prompt([
         {
             type: 'input',
             name: 'email',
             message: 'user.email',
             default: 'z_max_y@163.com',
-        },
+        }
     ])
     execSync(`git config user.name ${name}`)
     execSync(`git config user.email ${email}`)
-    console.log('设置完成，当前配置：\n')
+    console.log('设置完成，当前配置：')
     console.log(`git config user.name ${execSync('git config user.name')}`.trim())
     console.log(`git config user.email ${execSync('git config user.email')}`)
 }
