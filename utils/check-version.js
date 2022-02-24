438f9abea2af1112aae1a90d390c176548c9878d
streakingman <z_max_y@163.com>
2022-02-24 15:25:28 +0800

    fix: 优化控制台输出

--- utils/check-version.js
+++ utils/check-version.js
@@ -1,8 +1,10 @@
 const { execSync } = require('child_process');
 const { version } = require('../package.json');
 const checkVersion = () => {
-    const latestVersion = execSync('npm view streakingman-cli version');
-    const [x1, y1, z1] = latestVersion.toString().trim().split('.');
+    const latestVersion = execSync('npm view streakingman-cli version')
+        .toString()
+        .trim();
+    const [x1, y1, z1] = latestVersion.split('.');
     const [x2, y2, z2] = version.split('.');
     if (!(x1 === x2 && y1 === y2 && z1 <= z2)) {
         console.warn(
